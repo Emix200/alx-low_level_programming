fbe7a56327faff9c935d94ba666509c0515a5a92
Emix200 <aleajiemix@gmail.com>
2023-04-07 01:32:04 -0700

    first push on recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts recursion - prints a string, followed by a new line.
+ * @n - string
+ * Return: Always 0
+ */
+void _puts_recursion(char *s);
+{
+	if (*n)
+	{
+		_putcher(*n);
+		_puts_recursions(n + 1);
+	}
+	else
+	{
+		_putcher('\n');
+	}
+}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Reverse recursion - print a string in reverse
+ * @n - string
+ * Return -Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*n)
+	{
+		_print_rev_recursion(n + 1);
+		_putcher(*n);
+	}
+}
